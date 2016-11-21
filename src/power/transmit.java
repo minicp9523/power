f8a84d5f82c03a47ef8e3645a4342c809a374899
apple <apple@10.225.29.179>
2016-11-21 22:50:25 +0800

    V3

--- src/power/transmit.java
+++ src/power/transmit.java
@@ -22,7 +22,7 @@ public class transmit {
 		HTable table = new HTable(conf,TableName);
 		put.addColumn(Bytes.toBytes("Data"), Bytes.toBytes("ID"), Bytes.toBytes("THUC_001"));
 		put.addColumn(Bytes.toBytes("Data"), Bytes.toBytes("I"), Bytes.toBytes("0"));
-
+		//THIS
 		
 		table.close();
 	}

7bf84fbb95bb5f0be5c08523d053dc6cfee4d004
SC <SC@SC-PC_429>
2016-11-22 14:35:31 +0800

    get column

--- src/power/transmit.java
+++ src/power/transmit.java
@@ -14,25 +14,30 @@ public class transmit {
 	static Configuration conf = null;
 	static {
 		conf = HBaseConfiguration.create();
-		conf.set( "hbase.zookeeper.quorum", "master");
+		conf.set("hbase.zookeeper.quorum", "master");
 	}
-	
-	public static void addData(String rowKey,String TableName,String[] col1,String[] val1,String[] col2,String[] val2) throws IOException {
+
+	public static void addData(String rowKey, String TableName, String[] col1, String[] val1) throws IOException {
 		Put put = new Put(Bytes.toBytes(rowKey));
-		HTable table = new HTable(conf,TableName);
+		HTable table = new HTable(conf, TableName);
 		HColumnDescriptor[] columnFamilies = table.getTableDescriptor().getColumnFamilies();
-		
-		for(int i = 0; i < columnFamilies.length ; i++ ){
-			
+
+		for (int i = 0; i < columnFamilies.length; i++) {
+			String familyName = columnFamilies[i].getNameAsString();
+			if (familyName.equals("data")) {
+				for (int j = 0; j < columnFamilies.length; j++) {
+					put.add(Bytes.toBytes(familyName), Bytes.toBytes(col1[j]), Bytes.toBytes(val1[j]));
+					
+				}
+			}
 		}
 		put.addColumn(Bytes.toBytes("Data"), Bytes.toBytes("ID"), Bytes.toBytes("THUC_001"));
 		put.addColumn(Bytes.toBytes("Data"), Bytes.toBytes("I"), Bytes.toBytes("0"));
-		
-		
+
 		table.close();
 	}
-	
-	public static void main (String[] args) throws Exception{
-		
+
+	public static void main(String[] args) throws Exception {
+//		addData("data","power","col1","val1");
 	}
 }

5ea1153ba4480fe039f82145da06d495cbca46be
apple <apple@192.168.0.144>
2016-11-28 01:09:19 +0800

    insert and scan

--- src/power/transmit.java
+++ src/power/transmit.java
@@ -17,28 +17,58 @@ public class transmit {
 		conf.set("hbase.zookeeper.quorum", "master");
 	}
 
-	public static void addData(String rowKey, String TableName, String[] col1, String[] val1) throws IOException {
-		HTable table = new HTable(conf, TableName);
-		Put put = new Put(Bytes.toBytes(rowKey));		//³]¸mRowKey
-		HColumnDescriptor[] columnFamilies = table.getTableDescriptor().getColumnFamilies();
+	public static void addData(String rowKey, String TableName, String family, String qualifier, String value)
+			throws IOException {
+		try {
+			HTable table = new HTable(conf, TableName);
+			Put put = new Put(Bytes.toBytes(rowKey)); // ï¿½]ï¿½mRowKey
+			// HColumnDescriptor[] columnFamilies =
+			// table.getTableDescriptor().getColumnFamilies();
 
-		for (int i = 0; i < columnFamilies.length; i++) {
-			String familyName = columnFamilies[i].getNameAsString();
-			if (familyName.equals("col1")) {
-				for (int j = 0; j < columnFamilies.length; j++) {
-					put.add(Bytes.toBytes(familyName), Bytes.toBytes(col1[j]), Bytes.toBytes(val1[j]));
-					
+			// for (int i = 0; i < columnFamilies.length; i++) {
+			// String familyName = columnFamilies[i].getNameAsString();
+			// if (familyName.equals("col1")) {
+			// for (int j = 0; j < columnFamilies.length; j++) {
+			// put.add(Bytes.toBytes(familyName), Bytes.toBytes(col1[j]),
+			// Bytes.toBytes(val1[j]));
+			//
+			// }
+			// }
+			// }
+			put.add(Bytes.toBytes(family), Bytes.toBytes(qualifier), Bytes.toBytes(value));
+			table.put(put);
+			table.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void getAllRecord(String TableName, String rowKey) {
+		try {
+			HTable table = new HTable(conf, TableName);
+			Scan s = new Scan();
+			ResultScanner ss = table.getScanner(s);
+			for (Result r : ss) {
+				for (KeyValue kv : r.raw()) {
+					System.out.print(new String(kv.getRow()) + " ");
+					System.out.print(new String(kv.getFamily()) + ":");
+					System.out.print(new String(kv.getQualifier()) + " ");
+					System.out.print(kv.getTimestamp() + " ");
+					System.out.println(new String(kv.getValue()));
 				}
 			}
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		put.addColumn(Bytes.toBytes("Data"), Bytes.toBytes("ID"), Bytes.toBytes("THUC_001"));
-		put.addColumn(Bytes.toBytes("Data"), Bytes.toBytes("I"), Bytes.toBytes("0"));
-		
-		table.close();
 	}
 
 	public static void main(String[] args) throws Exception {
-		
-//		addData("data","power","col1","val1");
+		try{
+			String TableName = "power";
+			String family = "data";
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		// addData("data","power","col1","val1");
 	}
 }
